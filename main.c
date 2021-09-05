fb53521222ae1cc9a5e12ea1d1b73e75cb09799c
LadaSelivanova2001 <lvselivanova@miem.hse.ru>
2021-09-05 18:54:01 +0300

    .

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
+#define _CRT_SECURE_NO_WARNINGS 
 #include <stdio.h>  
 #include <stdlib.h>  
 #include <time.h>  
 #include <locale.h>  
+
 
 // Функция сортировки прямым выбором 
 void selectionSort(unsigned long* num, int size)
